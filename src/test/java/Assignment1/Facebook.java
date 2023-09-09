2a298ea089c29da9375d1cf4e140048f7b45ca0a
Adenike <agboolaadenike88@gmail.com>
2023-09-09 23:38:30 +0100

    automate post message

--- src/test/java/Assignment1/Facebook.java
+++ src/test/java/Assignment1/Facebook.java
@@ -1,6 +1,44 @@
 package Assignment1;
 
 import BaseClasses.TestBase;
+import Pages.FBCreatePostPage;
+import Pages.FBDashboardPage;
+import Pages.FBLoginPage;
+import org.openqa.selenium.Dimension;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.concurrent.TimeUnit;
 
 public class Facebook extends TestBase {
+
+    FBLoginPage loginPage;
+    FBDashboardPage dashboardPage;
+    FBCreatePostPage createPostPage;
+
+    @Test(priority = 1, description = "Verify user can log in successfully with valid credentials")
+    public void userLogin() {
+
+        loginPage = new FBLoginPage(driver);
+        driver.get(testData.getProperty("facebookURL"));
+        loginPage.enterEmail(testData.getProperty("facebookUsername"));
+        loginPage.enterPassword(testData.getProperty("facebookPassword"));
+        loginPage.clickLoginButton();
+        clearNotification();
+    }
+    @Test(priority = 2, description = "Verify user can click post menu")
+    public void userClickPostMenu() {
+        dashboardPage =new FBDashboardPage(driver);
+        dashboardPage.clickMenuIcon();
+        dashboardPage.clickPostMenu();
+    }
+
+    @Test(priority = 3, description = "Verify user can post a status message")
+    public void userPostMessage(){
+        createPostPage= new FBCreatePostPage(driver);
+        createPostPage.enterMessagePost(testData.getProperty("message"));
+        createPostPage.clickPostButton();
+    }
 }
